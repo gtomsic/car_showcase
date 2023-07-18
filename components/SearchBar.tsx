54a0ea7b43e46e622e3f05fab5dd36046d2b7100
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-07-18 11:53:34 -0500

    Initial commit with home hero and footer section

---
+++ components/SearchBar.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+
+function SearchBar() {
+  const handleSearch = () => {};
+  return <form className="searchbar" onSubmit={handleSearch}></form>;
+}
+
+export default SearchBar;
